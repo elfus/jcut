0c4d045707460e69231ae5211480a19137370a75
Adrian Ortega <elfus0.1@gmail.com>
2014-06-15 12:59:06 -0500

    Added tests folder along with  the first group of tests

---
+++ tests/groupA/test_group_a.c
@@ -0,0 +1,17 @@
+
+int sum(int a, int b) {
+	return a+b;
+}
+
+int subs(int a, int b) {
+	return a-b;
+}
+
+float mult(float a, float b) {
+	return a*b;
+}
+
+double div(double a, double b) {
+	return a/b;
+}
+
