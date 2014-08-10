61644258cc5881f06d8a8c49b759d2a6f62a7fa2
Adrian Ortega <elfus0.1@gmail.com>
2014-08-10 16:02:02 -0500

    Added a small bash script to run all the tests in this folder

---
+++ tests/run-tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This script has to be executed in the project folder <project-root>/tests
+#
+# It will iterate over all the folders in the tests folder assuming there is
+# always a C file named 'test_group.c' and a test file named test-file.txt
+#
+JIT_TESTING="$HOME/projects/build-release34/Release+Asserts/bin/jit-testing"
+
+DIRECTORIES=`ls -f | grep group`
+FAILED=0
+
+echo "Current dir: `pwd`"
+echo "JIT_TESTING: $JIT_TESTING"
+
+for dir in $DIRECTORIES
+do
+    echo "Running tests in folder $dir"
+    cd $dir
+    $JIT_TESTING test_group.c --test-file test-file.txt 2> stderr.txt 1> stdoutput.txt 
+    echo "Return code $?"
+    FAILED=$((FAILED+$?))
+    cd ..
+done
+
+echo "Number of failed tests: $FAILED"
